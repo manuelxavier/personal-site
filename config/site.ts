6c110f7678a6bfa1556c3e2d9d873bce69ab4583
alexandrexavier <jalexandre.xavier@gmail.com>
2025-03-10 15:58:53 +0000

    more content

---
+++ config/site.ts
@@ -0,0 +1,36 @@
+export const siteConfig = {
+  name: "Manuel Xavier Portfolio",
+  authorName: "Manuel Xavier",
+  username: "manuelxavier",
+  description:
+    "Manuel Xavier is a Full Stack Developer, who loves to build web applications and is passionate about learning new technologies.",
+  url: "https://nbarkiya.xyz",
+  links: {
+    twitter: "https://x.com/manuelxavier",
+    github: "https://github.com/manuelxavier",
+  },
+  ogImage: "https://manuelxavier.xyz/manel-img.jpg",
+  iconIco:
+    "https://res.cloudinary.com/dejnlqc1f/image/upload/v1741593504/light-bulb_nt17yr.ico",
+  logoIcon:
+    "https://res.cloudinary.com/dejnlqc1f/image/upload/v1741593504/light-bulb_fwmozq.png",
+  keywords: [
+    "Manuel Xavier",
+    "Manuel Xavier Portfolio",
+    "Manuel Xavier Blog",
+    "Manuel Xavier Projects",
+    "Manuel Xavier Experience",
+    "Manuel Xavier Resume",
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+    "Shadcn",
+    "Next SEO",
+    "Next Server Components",
+    "Next.js Portfolio",
+    "Next.js Blog",
+    "Next.js Starter",
+  ],
+};
---
+++ config/socials.ts
@@ -0,0 +1,35 @@
+import { Icons } from "@/components/common/icons";
+
+interface SocialInterface {
+  name: string;
+  username: string;
+  icon: any;
+  link: string;
+}
+
+export const SocialLinks: SocialInterface[] = [
+  {
+    name: "Github",
+    username: "@manuelxavier",
+    icon: Icons.gitHub,
+    link: "https://github.com/manuelxavier",
+  },
+  {
+    name: "LinkedIn",
+    username: "Manuel Xavier",
+    icon: Icons.linkedin,
+    link: "https://www.linkedin.com/in/manuelxavier",
+  },
+  {
+    name: "Twitter",
+    username: "@manuelxavier",
+    icon: Icons.twitter,
+    link: "https://twitter.com/manuelxavier",
+  },
+  {
+    name: "Gmail",
+    username: "naman.barkiya02",
+    icon: Icons.gmail,
+    link: "mailto:naman.barkiya02@gmail.com",
+  },
+];
